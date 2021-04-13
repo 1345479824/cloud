229fb1173264d3e631da86d6b79d87a8012df817
1345479824 <1345479824@qq.com>
2021-04-13 15:47:47 +0800

    网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/atguigu/filter/MyLogGateWayFilter.java
@@ -0,0 +1,37 @@
+package com.atguigu.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.lang.annotation.Annotation;
+import java.util.Date;
+
+//网关过滤器,配置了但是暂时没用，不知道为什么，后面在研究
+@Component
+@Slf4j
+public class MyLogGateWayFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        log.info("****come in filter : " + new Date());
+        String uname = exchange.getRequest().getQueryParams().getFirst("uname");
+        if (uname == null){
+            log.info("用户名为null，非法用户");
+            exchange.getResponse().setStatusCode(HttpStatus.NOT_ACCEPTABLE);
+            return exchange.getResponse().setComplete();
+        }
+        return chain.filter(exchange);
+    }
+
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
